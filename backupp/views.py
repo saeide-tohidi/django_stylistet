edca699e4b6b20bd7911d6fb0dcfbb0c2054693f
tohidi <s.tohidi22@gmail.com>
2022-12-07 01:00:28 +0330

    folder bug

--- backupp/views.py
+++ backupp/views.py
@@ -1,19 +1,17 @@
 from datetime import datetime
-
 from django.contrib import messages
 from django.core.management import call_command
 from django.core.files import File
 from django.http import HttpResponse, HttpResponseRedirect
-
 from backupp.models import BackupHistory
 
 
 def dump_view(request):
     filename = datetime.now().strftime("%y-%m-%d, %H:%M")
-    output = open(f"backups/dumps/{filename}.json", "w")
+    output = open(f"dumpfile/{filename}.json", "w")
     call_command("dumpdata", format="json", indent=3, stdout=output)
     output.close()
-    output = open(f"backups/dumps/{filename}.json")
+    output = open(f"dumpfile/{filename}.json")
     backup = BackupHistory()
     backup.name = filename
     backup.file = File(output)
